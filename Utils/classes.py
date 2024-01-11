a7f0903c979c3973964d5890b096d7a1c37805f9
Grade <aborecevskij94@gmail.com>
2024-01-11 13:15:54 +0200

    Updated channel callbacks

--- Utils/classes.py
+++ Utils/classes.py
@@ -24,16 +24,32 @@ class CallbackClasses:
         class AdminDeclineRequest(CallbackData, prefix="admin_decline_request"):
             user_id: int
 
+    class ChannelCallbacks:
+        class EditChannelsCallback(CallbackData, prefix="edit_channels"):
+            user_id: int
+
+        class AddChannelCallback(CallbackData, prefix="add_channel"):
+            user_id: int
+
+        class RemoveChannelCallback(CallbackData, prefix="remove_channel"):
+            user_id: int
+
+        class AddChannelDoneCallback(CallbackData, prefix="add_channel_done"):
+            user_id: int
+
+        class RemoveChannelDoneCallback(CallbackData, prefix="remove_channel_done"):
+            user_id: int
+            channel_id: int
+
+        class RemovePageCallback(CallbackData, prefix="page"):
+            user_id: int
+            page: int
+
+        class GetChannelRequestLink(CallbackData, prefix="get_channel_request_link"):
+            user_id: int
+            channel_id: int
+
     class DelayCallback(CallbackData, prefix="delay"):
-        user_id: int
-
-    class EditChannelsCallback(CallbackData, prefix="edit_channels"):
-        user_id: int
-
-    class AddChannelCallback(CallbackData, prefix="add_channel"):
-        user_id: int
-
-    class RemoveChannelCallback(CallbackData, prefix="remove_channel"):
         user_id: int
 
     class SettingsMenuCallback(CallbackData, prefix="settings_menu"):
@@ -41,18 +57,3 @@ class CallbackClasses:
 
     class CancelCallback(CallbackData, prefix="cancel"):
         user_id: int
-
-    class AddChannelDoneCallback(CallbackData, prefix="add_channel_done"):
-        user_id: int
-
-    class RemoveChannelDoneCallback(CallbackData, prefix="remove_channel_done"):
-        user_id: int
-        channel_id: int
-
-    class RemovePageCallback(CallbackData, prefix="page"):
-        user_id: int
-        page: int
-
-    class GetChannelRequestLink(CallbackData, prefix="get_channel_request_link"):
-        user_id: int
-        channel_id: int
