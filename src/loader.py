2cdbee1ba311094dfa3a71c7f0d9aa9025fecd56
Grade <aborecevskij94@gmail.com>
2024-01-15 17:59:57 +0200

    Rollback

--- src/loader.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
-
-TOKEN = '6955659259:AAG-u7y5th4dqkGn23JmyAcA6E2LbbzxYPU'
-bot = Bot(token=TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot=bot, storage=storage)
