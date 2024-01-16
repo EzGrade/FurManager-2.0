8a3d0174ecbbb7e7f3018aa12fea71c800c1b5d8
Grade <aborecevskij94@gmail.com>
2024-01-16 14:48:31 +0200

    Updated django standalone

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
+import django.conf
+
+import sys
 import os
 
-import django
+sys.path.append(os.path.abspath('django_orm'))
 
-os.path.abspath("django_orm")
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_orm.django_orm.settings')
-
+django.conf.settings.configure(
+    DATABASES={
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "furmanager",
+            "USER": "postgres",
+            "PASSWORD": "GradeTop1!",
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
+    },
+    INSTALLED_APPS=[
+        'rest_framework',
+        'PostsModel.apps.PostsmodelConfig',
+        'UserModel.apps.UsermodelConfig',
+        'ChannelModel.apps.ChannelmodelConfig',
+    ]
+)
 django.setup()
