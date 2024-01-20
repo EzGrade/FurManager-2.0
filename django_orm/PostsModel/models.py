a1239209e59afe9caea106fbb68f3a943aef563e
Grade <aborecevskij94@gmail.com>
2024-01-20 17:56:30 +0200

    Add full GIF support

--- django_orm/PostsModel/models.py
+++ django_orm/PostsModel/models.py
@@ -4,9 +4,14 @@ from ChannelModel.models import ChannelModel
 from UserModel.models import UserModel
 
 
-# Create your models here.
 class PostsModel(models.Model):
+    MEDIA_TYPES = [
+        ('PHOTO', 'Photo'),
+        ('GIF', 'Gif'),
+    ]
+
     photo = models.TextField(blank=True, null=True)
+    media_type = models.CharField(max_length=5, choices=MEDIA_TYPES, default='PHOTO')
     caption = models.TextField(blank=True, null=True)
     channels = models.ManyToManyField(ChannelModel, blank=True)
 
